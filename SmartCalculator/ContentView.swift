6bee65861108ea3c26fd3dd4b9807b52512e54a2
Andy Chen <chenyangpoppin@hotmail.com>
2025-04-15 15:55:17 +0800

    Initialize the app

---
+++ SmartCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SmartCalculator
+//
+//  Created by Yang Chen on 2025-04-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello Smart Calculator!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SmartCalculator/SmartCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartCalculatorApp.swift
+//  SmartCalculator
+//
+//  Created by Yang Chen on 2025-04-15.
+//
+
+import SwiftUI
+
+@main
+struct SmartCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
